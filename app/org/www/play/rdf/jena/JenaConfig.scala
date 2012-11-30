40265b76166b9e0991b7ee38b9b6d4c110985cea
Henry Story <henry.story@bblfish.net>
2012-11-30 19:35:00 +0100

    pointing from front page to services

--- app/org/www/play/rdf/jena/JenaConfig.scala
+++
@@ -1,6 +0,0 @@
-package org.www.play.rdf.jena
-
-import org.w3.banana.jena.{JenaRDFWriter, Jena}
-import controllers.setup._
-
-object CORSProxy extends org.www.readwriteweb.play.CORSProxy[Jena](jenaAsync.graphIterateeSelector,JenaRDFWriter.selector)
