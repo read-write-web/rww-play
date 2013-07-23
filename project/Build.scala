18c1f45246e4efde09c43d41df19ce006d1ea83b
Edward Silhol <edward@stample.co>
2013-07-23 20:05:09 +0200

    fix scala-stm dependency problem

--- project/Build.scala
+++ project/Build.scala
@@ -10,11 +10,11 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP8",
-      "org.w3" %% "banana-jena" % "2013_06_14-SNAPSHOT",
-      "org.w3" %% "banana-sesame" % "2013_06_14-SNAPSHOT",
-      "org.w3" %% "banana-rdf" % "2013_06_14-SNAPSHOT",
-      "org.w3" %% "plantain" % "2013_06_14-SNAPSHOT",
-      "org.w3" %% "ldp" % "2013_06_14-SNAPSHOT",
+      "org.w3" %% "banana-jena" % "2013_06_14-SNAPSHOT" excludeAll(ExclusionRule(organization = "org.scala-stm")),
+      "org.w3" %% "banana-sesame" % "2013_06_14-SNAPSHOT" excludeAll(ExclusionRule(organization = "org.scala-stm")),
+      "org.w3" %% "banana-rdf" % "2013_06_14-SNAPSHOT" excludeAll(ExclusionRule(organization = "org.scala-stm")),
+      "org.w3" %% "plantain" % "2013_06_14-SNAPSHOT" excludeAll(ExclusionRule(organization = "org.scala-stm")),
+      "org.w3" %% "ldp" % "2013_06_14-SNAPSHOT" excludeAll(ExclusionRule(organization = "org.scala-stm")),
       "net.rootdev" % "java-rdfa" % "0.4.2-RC2",
       "nu.validator.htmlparser" % "htmlparser" % "1.2.1",
       "org.scalaz" % "scalaz-core_2.10" % "7.0.0-RC1", // from "http://repo.typesafe.com/typesafe/releases/org/scalaz/scalaz-core_2.10.0-M6/7.0.0-M2/scalaz-core_2.10.0-M6-7.0.0-M2.jar"
