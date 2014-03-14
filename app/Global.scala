0c9ba98b82bf15f7eed4eded71c2e6bcacd5d4de
sebastien <lorber.sebastien@gmail.com>
2014-03-14 10:59:35 +0100

    Add OPTIONS route to Global RWW request routing for #113

--- app/Global.scala
+++ app/Global.scala
@@ -9,27 +9,32 @@ import play.api._
 import mvc.RequestHeader
 
 object Global extends GlobalSettings {
+
   override def onRouteRequest(req: RequestHeader) = {
     import rww.play.EnhancedRequestHeader
 
     val uri = req.getAbsoluteURI
-
     if (
       uri.getPath.startsWith("/assets/")
       || uri.getPath.startsWith("/srv/")
-      || uri.getHost().startsWith("www")
-    ) {
+      || uri.getHost().startsWith("www") ) {
       super.onRouteRequest(req)
-    } else if (uri.getHost != controllers.plantain.host) {
+    }
+    else if (uri.getHost != controllers.plantain.host) {
       req.method match {
         case "GET" => Some(ReadWriteWebController.get(req.path))
         case "POST" => Some(ldp.ReadWriteWebController.post(req.path))
         case "PATCH" => Some(ldp.ReadWriteWebController.patch(req.path))
         case "MKCOL" => Some(ldp.ReadWriteWebController.mkcol(req.path))
         case "HEAD" => Some(ldp.ReadWriteWebController.head(req.path))
+        case "OPTIONS" => Some(ReadWriteWebController.options(req.path))
         case "PUT" =>  Some(ldp.ReadWriteWebController.put(req.path))
         case "DELETE" => Some(ldp.ReadWriteWebController.delete(req.path))
       }
-    } else  super.onRouteRequest(req)
+    }
+    else {
+      super.onRouteRequest(req)
+    }
   }
+
 }
